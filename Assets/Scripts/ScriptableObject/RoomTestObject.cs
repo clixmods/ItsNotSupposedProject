18ec0efe25575b50cda39e6b1919058eeab6c487
clixmods <clix.mods.cod@gmail.com>
2022-03-19 19:23:17 +0100

    Init

---
+++ Assets/Scripts/ScriptableObject/RoomTestObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Level Data", menuName = "RoomTest/Level", order = 1)]
+public class RoomTestObject : ScriptableObject
+{
+    [Tooltip("Name of the level, it will be used for menu etc")]
+    public string Name;
+    [Tooltip("Description of the level, it will be used for menu etc")]
+    public string Description;
+    [Tooltip("PreviewImage of the level, it will be used for menu etc")]
+    public Sprite PreviewImage;
+    
+    public bool isUnlocked;
+    public string sceneName;
+
+}
